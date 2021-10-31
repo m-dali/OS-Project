451e759b2d4eb9cf03cf1f2be3f7cc850a5b37c9
m-dali <mukhammadaliadizov@gmail.com>
2021-10-31 18:56:26 +0500

    concurenncy added

--- client/client.py
+++ client/client.py
@@ -1,7 +1,7 @@
 #!/usr/bin/env python3
 import os
 import socket
-from threading import *
+from threading import Lock,Semaphore,Thread
 HOST = ''          # The server's hostname or IP address
 PORT = 2021        # The port used by the server
 
@@ -10,24 +10,22 @@ HOST_MSG = "127.0.0.1"#socket.gethostbyname(name)   #if you dont specify address
 PORT_MSG = 2022       # for client to listen to messages from server
 print(HOST_MSG)
 mu = Lock()
-DISCONNECT_S = Semaphore(1) # 
+DISCONNECT_S = Semaphore(1)
 DISCONNECT = False
-      
+QUIT = False
 
 def protocolMessage(data:str):
     if data=="":
         return "",[]
-    message = ""
     command=data.split()
     action = command[0]
     len_command = len(command)
     if len_command==1:
         if action in ["lu","lf","disconnect"]:
             message = action.upper()
-        elif action == "quit":
-            message = action.upper()
         else:
             print("command doesn't exist:", action)  
+            return '',[]
     elif len_command==2:
         if action == "read":
             if os.path.exists(command[1]):
@@ -54,8 +52,13 @@ def protocolMessage(data:str):
                 print("file doesn't exists,",command[1])
                 return "",[]
             message = f"{action.upper()} {command[1]} {command[2]}"    
+        else:
+            return '',[]
     else:
         print("command doesn't exist:", command)
+        return '',[]
+    message+='\n' #commands are always terminated by \n character   
+    
     return message,command
 
 def recieving_untill_special_char(special_char:str,s:socket.socket)->str:
@@ -68,27 +71,33 @@ def recieving_untill_special_char(special_char:str,s:socket.socket)->str:
     return full_string
 
 def sending_thread(s:socket.socket):
-    isUserConnected = False   
-    global DISCONNECT            
+    global HOST
+    isUserConnected = False
+    global DISCONNECT
+    global QUIT
     while True:
-        DISCONNECT_S.acquire()
-        if DISCONNECT == True:
-            print(DISCONNECT)
+        DISCONNECT_S.acquire() # no need for using semaphore if disconnect wasnt issued in port 2022
+        if DISCONNECT:
             isUserConnected = False
-            s.close()
-            s = socket.socket()
+            s.close() #old socket closed
+            if QUIT:
+                DISCONNECT_S.release()
+                break
+            s = socket.socket() #new created with the same name
+            HOST = ''
             DISCONNECT = False
         DISCONNECT_S.release()
-
         mu.acquire()
-        data = input("Write command:")
+        data = input(">")
+        if data == 'quit':
+            data = 'disconnect'
+            QUIT = True
+        if data == "disconnect":
+            DISCONNECT_S.acquire()
         message,command = protocolMessage(data)
         if message == "":
             mu.release()
             continue
-        if command[0] == "disconnect":
-            DISCONNECT_S.acquire()
-        
         if command[0]=="connect":
             if not isUserConnected:
                 try:
@@ -113,6 +122,9 @@ def sending_thread(s:socket.socket):
         print(f"Response Code: {response_code}")
         mu.release()
         if response_code == "ERROR" and command[0] == "connect":
+            isUserConnected = False
+            s.close()
+            s = socket.socket()
             print("connect again, the given user already registered",command[1])
             continue
         elif response_code == "OK" and command[0] == "connect":
@@ -121,7 +133,7 @@ def sending_thread(s:socket.socket):
             continue
         if response_code == "ERROR":
             continue
-        
+            
         if command[0] == "send":
             text = " ".join(command[2:]).replace('"','')
             size = len(text)
@@ -131,14 +143,14 @@ def sending_thread(s:socket.socket):
             print(users)
         elif command[0] == "read":
             filename = command[1]
-            size_str = recieving_untill_special_char(' ',s) 
+            size_str = recieving_untill_special_char(' ',s)
             with open(filename,"w") as f:
                 f.write(s.recv(int(size_str)).decode())
         elif command[0] == "write":
             filename = command[1]
             size = os.path.getsize(filename)
             with open(filename, 'r') as f:
-                s.sendall((f"{size} {f.read()}").encode())                    
+                s.sendall((f"{size} {f.read()}").encode())                   
         elif command[0] == "overread":
             filename = command[1]
             size_str = recieving_untill_special_char(' ',s)
@@ -170,24 +182,26 @@ def recieving_thread(s:socket.socket):
         with conn:
             command = recieving_untill_special_char('\n',conn)
             if command == "MESSAGE":
-                size_of_msg = recieving_untill_special_char(' ',conn) 
+                size_of_msg = recieving_untill_special_char(' ',conn)
                 msg = conn.recv(int(size_of_msg)).decode()
                 mu.acquire()
-                # print(addr)
-                print(f">>>>>Recieved message: {msg}")
+                print(f"[RECIEVED MESSAGE]: {msg}")
                 mu.release()
             elif command == "DISCONNECT":
                 DISCONNECT = True
                 DISCONNECT_S.release()
-            else:
-                continue
+                if QUIT:
+                    s.close()
+                    break
 recieving_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+recieving_socket.setsockopt( socket.SOL_SOCKET, socket.SO_REUSEADDR, 1 )
+
 sending_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-sending = Thread(target=sending_thread,args=(sending_socket,))     #sending proto messages and recieving server response
+sending = Thread(target=sending_thread,args=(sending_socket,))       #sending proto messages and recieving server response
 recieving = Thread(target=recieving_thread,args=(recieving_socket,)) #recieving messages from other clients from the server
 sending.start()
 recieving.start()
 sending.join()
-print("JOIN sending")
+print("sending")
 recieving.join()
-print("recv sending")
+print("recieving")
--- server/server.py
+++ server/server.py
@@ -1,20 +1,26 @@
 #!/usr/bin/env python3
 import socket
-from threading import *
+from threading import Lock,Semaphore,Thread
 import os
+import time
+import signal
+import sys
 
-# NUM_OF_USERS = 0
-# mu = Lock()
+
 ONLINE_USERS = dict()
-name = socket.gethostname()
+# name = socket.gethostname()
+#socket.gethostbyname(name)
+HOST_SND = "127.0.1.1"
 
-HOST = "127.0.1.1" #socket.gethostbyname(name)   
 
-PORT = 2021        # Port to listen on (non-privileged ports are > 1023)
+mu_RW = Lock() #mutex for RW
+READER_WRITER = dict()  # Readerwriter[filename] = RW
 
+
+
+PORT_SND = 2021        # PORT_SND to listen on (non-privileged ports are > 1023)
 PORT_MSG = 2022
-BUFF = 100
-print(HOST)
+print(HOST_SND)
 def recieving_untill_special_char(special_char:str,s:socket.socket)->str:
     full_string= ""
     while True:
@@ -24,29 +30,24 @@ def recieving_untill_special_char(special_char:str,s:socket.socket)->str:
         full_string+=char
     return full_string
 
+class RW:
+  def __init__(self, readcount, mu,write_mu):
+    self.readcount = readcount
+    self.mu = mu
+    self.write_mu = write_mu
 
-def clientThread(conn, addr):  
+
+def clientThread(conn, addr):
+    global UPDATING_FILES  
+    # global readcount,writecount,write_mu
     with conn:
         print('Connected by', addr)
         while True:
-            message = conn.recv(BUFF).decode()
-            if message == "QUIT":
-                exit()
+            message = recieving_untill_special_char('\n',conn)
             print(message)
             command = message.split()
-            if command[0] == "CONNECT":
-                global ONLINE_USERS
-                # global mu
-                if command[1] not in ONLINE_USERS:
-                    # mu.acquire()
-                    ONLINE_USERS[command[1]] = "127.0.0."+ str(len(ONLINE_USERS)+1)
-                    # mu.release()
-                    
-                    print(ONLINE_USERS[command[1]])
-                    conn.sendall(b"OK\n")
-                else:
-                    conn.sendall(b"ERROR\n")
-            elif command[0] == "LF":
+            
+            if command[0] == "LF":
                 conn.sendall(b"OK\n")
                 files = ""
                 for f in os.listdir('.'):
@@ -54,6 +55,20 @@ def clientThread(conn, addr):
                          files+=f+" "
                 files+="\n"
                 conn.sendall(files.encode())
+            elif command[0] == "DISCONNECT":
+                conn.sendall(b"OK\n")
+                print(ONLINE_USERS)
+                print(addr[0])
+                for key in ONLINE_USERS:
+                    # if ONLINE_USERS[key]==addr[0]:#use addr[0] here when it is locally available but in localhost it wont work
+                    del_addr = ONLINE_USERS.pop(key)
+                    print(ONLINE_USERS)
+                    break
+                print(del_addr)#unbound local variable
+                with socket.socket() as message_socket:
+                    message_socket.connect((del_addr,PORT_MSG))
+                    message_socket.sendall("DISCONNECT\n".encode())
+                    break
             elif command[0] == "LU":
                 conn.sendall(b"OK\n")
                 list_of_users = ""
@@ -73,6 +88,16 @@ def clientThread(conn, addr):
                     conn.sendall(b"ERROR\n")
             elif command[0] == "READ":
                 filename = command[1]
+                if filename not in READER_WRITER:
+                    READER_WRITER[filename] = RW(0,Lock(),Lock())
+                rw = READER_WRITER[filename]
+                rw.mu.acquire()
+                rw.readcount += 1
+                if rw.readcount == 1:
+                    rw.write_mu.acquire()
+                rw.mu.release()
+
+                
                 if os.path.exists(filename):
                     conn.sendall(b"OK\n")
                     size = os.path.getsize(filename)
@@ -80,8 +105,13 @@ def clientThread(conn, addr):
                         conn.sendall((f"{size} {f.read()}").encode())
                 else:
                     conn.sendall(b"ERROR\n")
+                rw.mu.acquire()
+                rw.readcount-=1
+                if rw.readcount == 0:
+                    rw.write_mu.release()
+                rw.mu.release()
             elif command[0]=="WRITE":
-                filename = command[1]
+                filename = command[1]         
                 if os.path.exists(filename):
                     conn.sendall(b"ERROR\n")
                 else:
@@ -91,6 +121,19 @@ def clientThread(conn, addr):
                         f.write(conn.recv(int(size_str)).decode())
             elif command[0] == "OVERREAD":
                 filename = command[1]
+                mu_RW.acquire()
+                if filename not in READER_WRITER:
+                    READER_WRITER[filename] = RW(0,Lock(),Lock())
+                rw = READER_WRITER[filename]
+                mu_RW.release()
+
+                rw.mu.acquire()
+                rw.readcount += 1
+                if rw.readcount == 1:
+                    rw.write_mu.acquire()
+                rw.mu.release()
+                
+
                 if os.path.exists(filename):
                     conn.sendall(b"OK\n") 
                     size = os.path.getsize(filename)
@@ -98,15 +141,34 @@ def clientThread(conn, addr):
                         conn.sendall((f"{size} {f.read()}").encode())
                 else:
                     conn.sendall(b"ERROR\n")
+                rw.mu.acquire()
+                rw.readcount-=1
+                if rw.readcount == 0:
+                    rw.write_mu.release()
+                rw.mu.release()
             elif command[0] == "OVERWRITE":
                 filename = command[1]
+                mu_RW.acquire()
+                if filename not in READER_WRITER:
+                    READER_WRITER[filename] = RW(0,Lock(),Lock())
+                rw = READER_WRITER[filename]
+                mu_RW.release()
+
+                rw.write_mu.acquire()
                 conn.sendall(b"OK\n")
             
                 size_str = recieving_untill_special_char(' ',conn)
                 with open(command[1],"w") as f: 
                     f.write(conn.recv(int(size_str)).decode())
+                rw.write_mu.release()
             elif command[0] == "APPEND":
                 filename = command[1]
+                mu_RW.acquire()
+                if filename not in READER_WRITER:
+                    READER_WRITER[filename] = RW(0,Lock(),Lock())
+                rw = READER_WRITER[filename]
+                mu_RW.release()
+                rw.write_mu.acquire()
                 if os.path.exists(filename):
                     conn.sendall(b"OK\n")
                     size_str = recieving_untill_special_char(' ',conn)
@@ -114,8 +176,16 @@ def clientThread(conn, addr):
                         f.write(conn.recv(int(size_str)).decode())
                 else:
                     conn.sendall(b"ERROR\n")
+                rw.write_mu.release()       
             elif command[0] == "APPENDFILE":
                 filename = command[-1]
+                mu_RW.acquire()
+                if filename not in READER_WRITER:
+                    READER_WRITER[filename] = RW(0,Lock(),Lock())
+                rw = READER_WRITER[filename]
+                mu_RW.release()
+                rw.write_mu.acquire()
+                time.sleep(5)
                 if os.path.exists(filename):
                     conn.sendall(b"OK\n")
                     size_str = recieving_untill_special_char(' ',conn)
@@ -123,27 +193,34 @@ def clientThread(conn, addr):
                         f.write(conn.recv(int(size_str)).decode())
                 else:
                     conn.sendall(b"ERROR\n")
-            elif command[0] == "DISCONNECT":
-                conn.sendall(b"OK\n")
-                for key in ONLINE_USERS:
-                    print(key)
-                    if ONLINE_USERS[key]==addr[0]:
-                        del_addr = ONLINE_USERS.pop(key)
-                        break
-                    
-                print(del_addr)
-                with socket.socket() as message_socket:
-                    message_socket.connect((del_addr,PORT_MSG))
-                    message_socket.sendall("DISCONNECT\n".encode())
-                    break
-
+                rw.write_mu.release()
+                
+def signal_handler(sig, frame):
+    print('You pressed Ctrl+C!')
+    sys.exit(0)
+signal.signal(signal.SIGINT, signal_handler)
 with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as s:
-    s.setsockopt( socket.SOL_SOCKET, socket.SO_REUSEADDR, 1 )
 
-    s.bind((HOST, PORT))
+    s.setsockopt( socket.SOL_SOCKET, socket.SO_REUSEADDR, 1 )
+    s.bind((HOST_SND, PORT_SND))
 
     while True:
         s.listen(1)
         conn, addr = s.accept()
+        message = recieving_untill_special_char('\n',conn)
+        command = message.split()
+        while True:
+            if command[0] == "CONNECT":
+                if command[1] not in ONLINE_USERS:
+                    # mu.acquire()
+                    ONLINE_USERS[command[1]] = "127.0.0."+ str(len(ONLINE_USERS)+1)
+                    # mu.release()
+                    
+                    print(ONLINE_USERS[command[1]])
+                    conn.sendall(b"OK\n")
+                    break
+                else:
+                    conn.sendall(b"ERROR\n")
         newClient = Thread(target=clientThread, args=(conn, addr))
         newClient.start()
+
